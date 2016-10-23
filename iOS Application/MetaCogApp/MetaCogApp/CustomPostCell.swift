b92ff50dc350a745061b44dc592ff46f2f1633bb
José Gutiérrez <guti7@users.noreply.github.com>
2016-10-23 10:55:37 -0700

    Initial Commit. xcode project

---
+++ iOS Application/MetaCogApp/MetaCogApp/CustomPostCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomPostCell.swift
+//  MetaCogApp
+//
+//  Created by Guti on 10/20/16.
+//  Copyright © 2016 PielDeJaguar. All rights reserved.
+//
+
+import UIKit
+
+class CustomPostCell: UITableViewCell {
+    
+    var titlePost: String?
+    var contentPost: String?
+    var comments: [String]?
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iOS Application/MetaCogApp/MetaCogApp/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  MetaCogApp
+//
+//  Created by Guti on 10/20/16.
+//  Copyright © 2016 PielDeJaguar. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath)
+        
+        return cell
+    }
+
+}
