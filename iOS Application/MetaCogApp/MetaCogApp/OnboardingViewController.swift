d4f1174e4920961a41e07932b3211d93997f9294
GFV450 <gfv450@gmail.com>
2016-11-01 16:11:59 -0700

    Added labels, constraints and tab bar controller to Onboarding screen

---
+++ iOS Application/MetaCogApp/MetaCogApp/OnboardingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingViewController.swift
+//  MetaCogApp
+//
+//  Created by Gian Vitola on 11/1/16.
+//  Copyright © 2016 PielDeJaguar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OnboardingViewController: UIViewController {
+}
